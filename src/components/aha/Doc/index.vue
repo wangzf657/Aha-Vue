79103d119a676ca85e76aabe6e5e543e7f82e3d4
wangzhifa <wangzhifa@ihicon.com>
2024-10-31 15:33:12 +0800

    初始化

---
+++ src/components/aha/Doc/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="question" @click="goto" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ahaDoc',
+  data() {
+    return {
+      url: 'http://doc.aha.vip/aha-cloud'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
---
+++ src/components/aha/Git/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="github" @click="goto" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ahaGit',
+  data() {
+    return {
+      url: 'https://gitee.com/y_project/aha-Cloud'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
